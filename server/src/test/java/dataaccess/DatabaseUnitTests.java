0e36947fefa89e8bdec2df210a3e443f56bd91c0
fire8811 <jensonjones2002@gmail.com>
2025-03-12 20:57:55 -0600

    Finish all personal tests

--- server/src/test/java/dataaccess/DatabaseUnitTests.java
+++ server/src/test/java/dataaccess/DatabaseUnitTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import javax.xml.crypto.Data;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,117 +33,158 @@ public class DatabaseUnitTests {
     }
 
     //userSQL tests
-    @Test void testClearUsers() throws SQLException, DataAccessException {
+    @Test
+    void testClearUsers() throws SQLException, DataAccessException {
         userDatabase.addUser(new UserData("yeee", "haww", "email@email.com"));
         assertDoesNotThrow(userDatabase::clearUsers);
     }
 
-    @Test void testAddUser() {
+    @Test
+    void testAddUser() {
         assertDoesNotThrow(() -> userDatabase.addUser(new UserData("jonbob", "banana", "email@email.com")));
     }
 
-    @Test void testAddUserFail() {
+    @Test
+    void testAddUserFail() {
         assertThrows(ResponseException.class, () -> userDatabase.addUser(new UserData(null, "oooo", "email@email.com")));
     }
 
-    @Test void testIsUsernameFree() {
+    @Test
+    void testIsUsernameFree() {
         assertDoesNotThrow(() -> userDatabase.isUsernameFree("username"));
     }
 
-    @Test void testIsUsernameFreeFail() throws SQLException, DataAccessException {
+    @Test
+    void testIsUsernameFreeFail() throws SQLException, DataAccessException {
         userDatabase.addUser(new UserData("username", "password", "email"));
         assertThrows(UsernameTakenException.class, () -> userDatabase.isUsernameFree("username"));
     }
 
-    @Test void testFindUser() throws DataAccessException, SQLException {
+    @Test
+    void testFindUser() throws DataAccessException, SQLException {
         userDatabase.addUser(new UserData("jonbob", "banana", "email"));
         assertTrue(userDatabase.findUser("jonbob", "banana"));
     }
 
-    @Test void testFindUserFail() throws SQLException, DataAccessException {
+    @Test
+    void testFindUserFail() throws SQLException, DataAccessException {
         userDatabase.addUser(new UserData("jonbob1", "banana", "email"));
         assertThrows(UnauthorizedException.class, () -> userDatabase.findUser("jonbob1", "apple"));
     }
 
     //authSQL tests
-    @Test void testClearAuths() throws SQLException, DataAccessException {
+    @Test
+    void testClearAuths() throws SQLException, DataAccessException {
         authDatabase.addAuthData(new AuthData("token", "username"));
         assertDoesNotThrow(authDatabase::clearAuths);
     }
 
-    @Test void testAddAuthData() {
+    @Test
+    void testAddAuthData() {
         assertDoesNotThrow(() -> authDatabase.addAuthData(new AuthData("token", "jonbob")));
     }
 
-    @Test void testAddAuthDataFail() {
+    @Test
+    void testAddAuthDataFail() {
         assertThrows(ResponseException.class, () -> authDatabase.addAuthData(new AuthData(null, "username")));
     }
 
-    @Test void testDeleteAuthData() throws DataAccessException, SQLException {
+    @Test
+    void testDeleteAuthData() throws DataAccessException, SQLException {
         AuthData authData = new AuthData(UUID.randomUUID().toString(), "bruno");
         authDatabase.addAuthData(authData);
         assertDoesNotThrow(() -> authDatabase.deleteAuthData(authData.authToken()));
     }
 
-    @Test void testDeleteAuthDataFail() throws DataAccessException, SQLException {
-        assertThrows(UnauthorizedException.class, ()-> authDatabase.deleteAuthData("non-existent-token"));
+    @Test
+    void testDeleteAuthDataFail() throws DataAccessException, SQLException {
+        assertThrows(UnauthorizedException.class, () -> authDatabase.deleteAuthData("non-existent-token"));
     }
 
-    @Test void testAuthTokenExists() throws SQLException, DataAccessException {
+    @Test
+    void testAuthTokenExists() throws SQLException, DataAccessException {
         AuthData authData = new AuthData("authToken", "bruno");
         authDatabase.addAuthData(authData);
 
         assertDoesNotThrow(() -> authDatabase.authTokenExists("authToken"));
     }
 
-    @Test void testAuthTokenExistsFail() {
+    @Test
+    void testAuthTokenExistsFail() {
         assertThrows(UnauthorizedException.class, () -> authDatabase.authTokenExists("non-existent-token"));
     }
 
-    @Test void testGetUsername() throws SQLException, DataAccessException {
+    @Test
+    void testGetUsername() throws SQLException, DataAccessException {
         AuthData authData = new AuthData("authToken", "jonbob");
         authDatabase.addAuthData(authData);
-        assertDoesNotThrow(()-> authDatabase.getUsername("authToken"));
+        assertDoesNotThrow(() -> authDatabase.getUsername("authToken"));
     }
 
-    @Test void testGetUsernameFail() {
-        assertThrows(UnauthorizedException.class, ()-> authDatabase.getUsername("non-existent-token"));
+    @Test
+    void testGetUsernameFail() {
+        assertThrows(UnauthorizedException.class, () -> authDatabase.getUsername("non-existent-token"));
     }
 
+    //game SQL tests below
+    @Test
+    void testClearGames() throws SQLException, DataAccessException {
+        gameDatabase.createGame("gameName");
+        Collection<GameData> gameDataList = gameDatabase.listGames();
+        assertEquals(1, gameDataList.size());
 
-
-
-
-    @Test void testAddAuthdata() throws DataAccessException, SQLException {
-        authDatabase.addAuthData(new AuthData(UUID.randomUUID().toString(), "jonbob"));
-    }
-
-
-
-    @Test void testClearSQL() throws SQLException, DataAccessException {
-        authDatabase.clearAuths();
-        userDatabase.clearUsers();
         gameDatabase.clearGames();
+        gameDataList = gameDatabase.listGames();
+        assertEquals(0, gameDataList.size());
     }
 
-    @Test void testTokenFinding() throws DataAccessException {
-        assertTrue(authDatabase.authTokenExists("91a3d9f6-b426-4a0e-aaef-6af4590548c3"));
+    @Test
+    void testCreateGame() throws SQLException, DataAccessException {
+        assertEquals(1, gameDatabase.createGame("GameName"));
     }
 
-    @Test void testGetUsernameFromAuth() throws SQLException, DataAccessException {
-        assertEquals("jonbob", authDatabase.getUsername("91a3d9f6-b426-4a0e-aaef-6af4590548c3"));
+    @Test
+    void testCreateGameFail() {
+        assertThrows(ResponseException.class, () -> gameDatabase.createGame(null));
     }
 
-    @Test void testCreateGameSQL() throws SQLException, DataAccessException {
-        gameDatabase.createGame("juego");
+    @Test
+    void testFindGame() throws SQLException, DataAccessException {
+        gameDatabase.createGame("game");
+        assertTrue(gameDatabase.findGame(1));
     }
 
-    @Test void testListGamesSQL() throws DataAccessException {
-        ArrayList<GameData> gamesList = (ArrayList<GameData>) gameDatabase.listGames();
-        assertEquals(1, gamesList.size());
+    @Test
+    void testFindGameFail() throws DataAccessException {
+        assertFalse(gameDatabase.findGame(1));
     }
 
-    @Test void testJoinGameSQL() throws DataAccessException {
-        gameDatabase.updateGame(1, ChessGame.TeamColor.WHITE, "jonbob");
+    @Test
+    void testListGames() throws SQLException, DataAccessException {
+        gameDatabase.createGame("game1");
+        gameDatabase.createGame("game2");
+
+        Collection<GameData> gamesList = gameDatabase.listGames();
+
+        assertEquals(2, gamesList.size());
     }
-}
+
+    @Test
+    void testListGames2() throws DataAccessException {
+        Collection<GameData> gamesList = gameDatabase.listGames();
+        assertEquals(0, gamesList.size());
+    }
+
+    @Test
+    void testUpdateGame() throws SQLException, DataAccessException {
+        gameDatabase.createGame("game");
+        assertDoesNotThrow(() -> gameDatabase.updateGame(1, ChessGame.TeamColor.BLACK, "username"));
+    }
+
+    @Test
+    void testUpdateGameFail() throws SQLException, DataAccessException {
+        gameDatabase.createGame("a_game");
+        gameDatabase.updateGame(1, ChessGame.TeamColor.BLACK, "player1");
+
+        assertThrows(AlreadyTakenException.class, () -> gameDatabase.updateGame(1, ChessGame.TeamColor.BLACK, "player2"));
+    }
