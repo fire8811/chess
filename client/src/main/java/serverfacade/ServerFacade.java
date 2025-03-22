08abb66d318defa9a4539988fd3ab8fb9eeba085
fire8811 <jensonjones2002@gmail.com>
2025-03-22 15:55:05 -0600

    Finish untested server facade

--- client/src/main/java/serverfacade/ServerFacade.java
+++ client/src/main/java/serverfacade/ServerFacade.java
@@ -3,8 +3,7 @@ package serverfacade;
 import com.google.gson.Gson;
 import exceptions.DataAccessException;
 import exceptions.ResponseException;
-import model.RegisterRequest;
-import model.RegisterResult;
+import model.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,13 +11,58 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.*;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ServerFacade {
     private final String serverUrl;
 
     public ServerFacade(String url) {serverUrl = url;}
 
-    private <T> T sendRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+    public void clearServer(){
+        var path = "/db";
+        this.sendRequest("DELETE", path, null, null, null);
+    }
+
+    public RegisterResult registerUser(RegisterRequest request){
+        var path = "/user";
+        return this.sendRequest("POST", path, request, RegisterResult.class, null);
+    }
+
+    public LoginResult loginUser(LoginRequest request) {
+        var path = "/session";
+        return this.sendRequest("POST", path, request, LoginResult.class, null);
+    }
+
+    public void logoutUser(){
+        var path = "/session";
+        this.sendRequest("DELETE", path, null, null, null);
+    }
+
+    public ListResult listGames(ListRequest request){
+        var path = "/game";
+        Map<String, String> header = new HashMap<>();
+        header.put("authorization", request.authToken()); //place the authtoken in a request header instead of the body
+
+        return this.sendRequest("GET", path, null, ListResult.class, header);
+    }
+
+    private CreateResult createGame(CreateRequest request){
+        var path = "/game";
+        Map<String, String> header = new HashMap<>();
+        header.put("authorization", request.authToken());
+
+        return this.sendRequest("POST", path, request, CreateResult.class, header);
+    }
+
+    private JoinResult joinGame(JoinRequest request){
+        var path = "/game";
+        Map<String, String> header = new HashMap<>();
+        header.put("authorization", request.authToken());
+
+        return this.sendRequest("PUT", path, request, JoinResult.class, header);
+    }
+
+    private <T> T sendRequest(String method, String path, Object request, Class<T> responseClass, Map<String, String> header) throws ResponseException {
         try {
             URL url = (new URI(serverUrl + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
@@ -29,14 +73,25 @@ public class ServerFacade {
                 writeHttpBody(request, http); //write JSON body
             }
 
+            if (header != null) { //add header to request (usually for sending authTokens)
+                writeHeader(header, http);
+            }
+
             http.connect();
             throwIfNotSuccessful(http);
+
             return readFromBody(http, responseClass);
 
         } catch (ResponseException e) {
             throw e;
         } catch (Exception e) {
             throw new ResponseException(e.getMessage());
+        }
+    }
+
+    private static void writeHeader(Map<String, String> header, HttpURLConnection http) throws IOException {
+        for (var entry: header.entrySet()){
+            http.setRequestProperty(entry.getKey(), entry.getValue());
         }
     }
 
