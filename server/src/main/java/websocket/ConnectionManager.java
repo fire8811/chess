0228359fc98dee17306dad74c37c865c1b85b58f
fire8811 <jensonjones2002@gmail.com>
2025-04-09 17:32:14 -0600

    setup connection manager and connection class

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -1,4 +1,38 @@
 package websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String name, Session session){
+        var connection = new Connection(name, session);
+        connections.put(name, connection);
+    }
+
+    public void remove(String visitorName) { connections.remove(visitorName); }
+
+    public void broadcast(String nameToExclude, ServerMessage message) throws IOException {
+        var deadConnections = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!c.name.equals(nameToExclude)) {
+                    c.send(message.toString());
+                }
+            }
+            else {
+                deadConnections.add(c);
+            }
+        }
+
+        //clean up connections left open
+        for (var c: deadConnections){
+            connections.remove(c.name);
+        }
+    }
 }
